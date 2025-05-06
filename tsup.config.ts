fdace728ecb1600b595ff6b15c7047a57cd5d14c
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-05-06 05:46:14 -0300

    feat: setup project
    - Update package.json with project details, scripts, and dependencies
    - Configure tsup for building the library
    - Set initial version to 0.0.1

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  minify: true,
   clean: true,
   dts: true,
-  minify: true,
 })
