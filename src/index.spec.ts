6a9b684491d67273a592af760da13cbc114370f2
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-05-07 04:54:08 -0300

    test: remove test case

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,5 @@
-import { greet } from './index.ts'
+// import { greet } from './index.ts'
 
-test('greet function', () => {
-  expect(greet('World')).toBe('Hello, World!')
-})
+// test('greet function', () => {
+//   expect(greet('World')).toBe('Hello, World!')
+// })
