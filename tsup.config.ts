6dc7d8a7b4fa531917b184097cb45cbbfbf44b6c
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-05-06 23:44:11 -0300

    feat: configure tsup build process
    - Update tsup configuration for enhanced build process
    - Add minification based on NODE_ENV
    - Configure output extensions for both cjs and esm formats
    - Add a banner to the output files with package information

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,34 @@
-import { defineConfig } from 'tsup'
+import { defineConfig, type Format } from 'tsup'
+import packageJson from './package.json'
+
+const shouldMinify = process.env.NODE_ENV === 'production'
+
+const extensions: Record<'esm' | 'cjs', string> = {
+  esm: shouldMinify ? '.min.mjs' : '.mjs',
+  cjs: shouldMinify ? '.min.cjs' : '.cjs',
+}
+
+const banner = `
+/**
+ * ${packageJson.name} v${packageJson.version}
+ * ${packageJson.description}
+ *
+ * @license ${packageJson.license}
+ * @copyright ${new Date().getFullYear()} ${packageJson.author}
+ * @see ${packageJson.homepage}
+ */`
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['cjs', 'esm'],
-  minify: true,
+  entry: ['./src/index.ts'],
+  format: Object.keys(extensions) as Format[],
+  minify: shouldMinify,
   clean: true,
   dts: true,
+  treeshake: true,
+  outExtension: ({ format }) => ({
+    js: extensions[format],
+  }),
+  banner: {
+    js: banner,
+  },
 })
