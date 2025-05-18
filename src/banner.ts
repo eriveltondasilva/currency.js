b130919a3649f142354551284caf78693b7cca7d
Erivelton Silva <eriveltondasilva13@gmail.com>
2025-05-18 01:47:57 -0300

    feat: add banner and remove example file

---
+++ src/banner.ts
@@ -0,0 +1,11 @@
+/**
+ * Biblioteca especializada para manipulação de valores monetários,
+ * evitando problemas de arredondamento com números de ponto flutuante e
+ * oferecendo uma API rica para operações financeiras.
+ *
+ * @author Erivelton Silva
+ * @version 0.0.1
+ * @license MIT
+ * @see https://github.com/erivelton/money-ts
+ * @see https://www.npmjs.com/package/money-ts
+ */
